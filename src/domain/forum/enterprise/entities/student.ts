2487613c8d416d9ab4c18c83211c6911001aa8b1
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-08-21 21:40:00 -0300

    feat: gateways cryptography

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,9 +3,24 @@ import { UniqueEntityId } from '@/core//entities/unique-entityId'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
+
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
   static create(props: StudentProps, id?: UniqueEntityId) {
     const student = new Student(props, id)
 
